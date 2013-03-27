7cd0c3003e5910db742ecb3e03f4db43549d1fe6
caoyixuan <caoyixuan1993@gmail.com>
2013-03-27 18:12:59 +0800

    new test

--- test.py
+++ test.py
@@ -14,8 +14,9 @@ class TestFixedrecipeItem(unittest.TestCase):
         app.config['TESTING'] = True
         db.drop_all()
         db.create_all()
-        #add basic data to database
         self.client = app.test_client()
+
+        #add basic data to database
         new_drug = drug(DRUGid='e28b7d9c-e817-47c1-b227-97d8eca021a7',\
                         code='2062', name='杏仁', unit='克', alias=' ',\
                         py='xr', wb='sw', isClassical='1', SPETid=' ',\
@@ -37,31 +38,9 @@ class TestFixedrecipeItem(unittest.TestCase):
 
         db.session.commit()
 
-    def tearDown(self):
-        db.drop_all()
+#    def tearDown(self):
+#        db.drop_all()
 
-    def test_fixedrecipe_get_all(self):
-        r = self.client.get('/api/fixedrecipe')
-        self.assertEqual(json.loads(r.data)['num_results'], 1)
-
-    def test_fixedrecipe_add(self):
-        r = self.client.post('/api/fixedrecipe',\
-                    data='{"FREPid":"test-e28b7d9c-e817-47c1-b227-97d8eca",\
-                        "code":"386","name":"test-abcfiexedrecipe","effect":"",\
-                        "py":"JWQHT","wb":"VKUII","isClassical":"1","SPETid":"",\
-                        "illustration":"","createDay":"1990-1-1 00:00:00",\
-                        "optrid":"c6543358-354b-4986-a06f-bd61b0cde15d","state":"3"}')
-        r = self.client.get('/api/fixedrecipe')
-        self.assertEqual(json.loads(r.data)['num_results'], 2)
-
-    def test_fixedrecipe_delete(self):
-        r = self.client.delete('/api/fixedrecipe/65dd5753-aad0-4261-94d3-09a67bd4f9a1')
-        r = self.client.get('/api/fixedrecipe')
-        self.assertEqual(json.loads(r.data)['num_results'], 0)
-        #the fixedrecipeItem shoule be deleted too
-        r = self.client.get('/api/fixedrecipe_item')
-        self.assertEqual(json.loads(r.data)['num_results'], 0)
-       
 
     def test_drug_get_all(self):
         r = self.client.get('/api/drug')
@@ -90,9 +69,41 @@ class TestFixedrecipeItem(unittest.TestCase):
         self.assertEqual(json.loads(r.data)['num_results'], 0)
         
 
+    def test_fixedrecipe_get_all(self):
+        r = self.client.get('/api/fixedrecipe')
+        self.assertEqual(json.loads(r.data)['num_results'], 1)
+
+    def test_fixedrecipe_get_by_id(self):
+        r = self.client.get('/api/fixedrecipe/65dd5753-aad0-4261-94d3-09a67bd4f9a1')
+        self.assertEqual(json.loads(r.data)['FREPid'], '65dd5753-aad0-4261-94d3-09a67bd4f9a1')
+
+    def test_fixedrecipe_add(self):
+        r = self.client.post('/api/fixedrecipe',\
+                    data='{"FREPid":"test-e28b7d9c-e817-47c1-b227-97d8eca",\
+                        "code":"386","name":"test-abcfiexedrecipe","effect":"",\
+                        "py":"JWQHT","wb":"VKUII","isClassical":"1","SPETid":"",\
+                        "illustration":"","createDay":"1990-1-1 00:00:00",\
+                        "optrid":"c6543358-354b-4986-a06f-bd61b0cde15d","state":"3"}')
+        r = self.client.get('/api/fixedrecipe')
+        self.assertEqual(json.loads(r.data)['num_results'], 2)
+
+    def test_fixedrecipe_delete(self):
+        r = self.client.delete('/api/fixedrecipe/65dd5753-aad0-4261-94d3-09a67bd4f9a1')
+        r = self.client.get('/api/fixedrecipe')
+        self.assertEqual(json.loads(r.data)['num_results'], 0)
+        #the fixedrecipeItem shoule be deleted too
+        r = self.client.get('/api/fixedrecipe_item')
+        self.assertEqual(json.loads(r.data)['num_results'], 0)
+       
+
     def test_fixedrecipeItem_get_all(self):
         r = self.client.get('/api/fixedrecipe_item')
         self.assertEqual(json.loads(r.data)['num_results'], 1)
+
+    def test_fixedrecipeItem_get_by_id(self):
+        r = self.client.get('/api/fixedrecipe_item/9c18a29f-faa1-494d-839c-7a89cba439e8')
+        print r.data
+        self.assertEqual(json.loads(r.data)['FRITid'], '9c18a29f-faa1-494d-839c-7a89cba439e8')
 
     def test_fixedrecipeItem_add(self):
         r = self.client.post('/api/fixedrecipe_item',\
